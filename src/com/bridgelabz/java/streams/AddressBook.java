039786f7c83afcfb1b8e78bc92216eba9156f49b
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-10-04 15:11:34 +0530

    Added: Create A Contact In Address Book

--- src/com/bridgelabz/java/streams/AddressBook.java
+++ src/com/bridgelabz/java/streams/AddressBook.java
@@ -1,10 +1,120 @@
 package com.bridgelabz.java.streams;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Contact {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "First Name='" + firstName + '\'' +
+                ", Last Name='" + lastName + '\'' +
+                ", Address='" + address + '\'' +
+                ", City='" + city + '\'' +
+                ", State='" + state + '\'' +
+                ", Zip='" + zip + '\'' +
+                ", Phone='" + phoneNumber + '\'' +
+                ", Email='" + email + '\'' +
+                '}';
+    }
+}
+
+class AddressBookInfo {
+
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public void displayContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("No contacts to display.");
+        } else {
+            for (Contact contact : contacts) {
+                System.out.println(contact);
+            }
+        }
+    }
+}
+
+
 public class AddressBook {
 
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Address Book Program");
+        Scanner scanner = new Scanner(System.in);
+        AddressBookInfo addressBook = new AddressBookInfo();
 
+        while (true) {
+            System.out.println("Address Book Menu:");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Display All Contacts");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // Consume newline
+
+            switch (choice) {
+                case 1:
+                    // Adding a new contact
+                    System.out.print("Enter First Name: ");
+                    String firstName = scanner.nextLine();
+                    System.out.print("Enter Last Name: ");
+                    String lastName = scanner.nextLine();
+                    System.out.print("Enter Address: ");
+                    String address = scanner.nextLine();
+                    System.out.print("Enter City: ");
+                    String city = scanner.nextLine();
+                    System.out.print("Enter State: ");
+                    String state = scanner.nextLine();
+                    System.out.print("Enter Zip Code: ");
+                    String zip = scanner.nextLine();
+                    System.out.print("Enter Phone Number: ");
+                    String phoneNumber = scanner.nextLine();
+                    System.out.print("Enter Email: ");
+                    String email = scanner.nextLine();
+
+                    Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+                    addressBook.addContact(contact);
+                    System.out.println("Contact added successfully!");
+                    break;
+
+                case 2:
+                    // Display all contacts
+                    addressBook.displayContacts();
+                    break;
+
+                case 3:
+                    // Exit the program
+                    System.out.println("Exiting Address Book. Goodbye!");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Please choose again.");
+            }
+        }
     }
+
 }
