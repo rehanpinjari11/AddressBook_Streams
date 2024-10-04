71b2afab5123d2a8b7f7bdaf4278df2a157f64fd
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-10-04 14:51:41 +0530

    Added: Welcome Message

---
+++ src/com/bridgelabz/java/streams/AddressBook.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.java.streams;
+
+public class AddressBook {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Address Book Program");
+
+    }
+}
